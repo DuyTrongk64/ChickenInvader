2f0fc87a33b20dd6f32d256d379e92785ec58ab4
DuyTrongk64 <duytrongk64@gmail.com>
2022-11-25 15:13:25 +0700

    update chicken and player

--- ChickenInvader/Chickens.cpp
+++ ChickenInvader/Chickens.cpp
@@ -1,2 +1,84 @@
-
 #include "Chickens.h"
+
+
+void Checkens::initVariables()
+{
+	this->pointCount = 5; //min = 3 max = 10
+	this->type = 0;
+	this->speed = 0.3f;
+	this->hpMax = static_cast<int>(this->pointCount);
+	this->hp = this->hpMax;
+	this->damage = this->pointCount;
+	this->points = this->pointCount;
+}
+
+void Checkens::initTexture()
+{
+	//Load a texture from file
+	if (!this->texture.loadFromFile("Textures/chicken.png"))
+	{
+		std::cout << "ERROR::PLAYER::INITTEXTURE::Could not load texture file." << "\n";
+	}
+}
+
+void Checkens::initSprite(const sf::RenderWindow& window)
+{
+	//Set the texture to the sprite
+	this->sprite.setTexture(this->texture);
+
+	//Resize the sprite
+	this->sprite.scale(0.1f, 0.1f);
+
+	this->sprite.setPosition(sf::Vector2f(static_cast<float>(rand() % window.getSize().x - this->sprite.getGlobalBounds().width),0.f));
+}
+
+
+
+Checkens::Checkens(const sf::RenderWindow& window)
+{
+	this->initVariables();
+	this->initTexture();
+	this->initSprite(window);
+}
+
+Checkens::~Checkens()
+{
+
+}
+
+//Accessors
+const sf::FloatRect Checkens::getBounds() const
+{
+	return this->sprite.getGlobalBounds();
+}
+
+const int& Checkens::getPoints() const
+{
+	return this->points;
+}
+
+const int& Checkens::getDamage() const
+{
+	return this->damage;
+}
+
+void Checkens::setPosition(const sf::Vector2f pos)
+{
+	this->sprite.setPosition(pos);
+}
+
+void Checkens::setPosition(const float x, const float y)
+{
+	this->sprite.setPosition(x, y);
+}
+
+//Functions
+void Checkens::update()
+{
+	this->sprite.move(0.f, this->speed);
+}
+
+void Checkens::render(sf::RenderTarget* target)
+{
+	target->draw(this->sprite);
+}
